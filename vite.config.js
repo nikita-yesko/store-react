7ea0ef2f669f5944f0cb23b77160981286ec3795
Brick <eskonikita@gmail.com>
2023-10-22 10:32:16 +0300

    7

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://nick-brick.github.io/store-react/",
+  base: "/store-react/",
   plugins: [react()],
 })
